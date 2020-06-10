a8b868679297527e589367189830f76e0a3cd62b
py-thony <byamungutony@gmail.com>
2020-06-10 22:20:16 +0200

    edited the sample python files

--- code/oopversion.py
+++ code/oopversion.py
@@ -17,34 +17,36 @@ class login:
         self.background = Label(self.master, image=self.bg_img)
         self.background.place(relheight=1, relwidth=1) 
 
-        # self.user_label = Label(self.background, text="Username", pady=20)
-        # self.user_label.grid(row=0, column=0, sticky=W)
-
-
-        self.frame1 = Frame(self.master, bg="black", bd=10)
-        self.frame1.place(relx=0.5, rely=0.1, relwidth=0.9, relheight=0.1, anchor='n')
-
         
-        self.user_input = Entry(self.frame1, bg="white", font=40)
-        self.user_input.place( relwidth=0.45, relheight=1)
-        self.user_input.insert(0, 'username')
 
-        self.pass_input = Entry(self.frame1, bg="white", font=40)
-        self.pass_input.place(relx = 0.55, rely=0,relwidth=0.45, relheight=1)
-        self.pass_input.insert(0, '***********')
+
+
+        self.frame1 = Frame(self.master, bg="black")
+        self.frame1.pack(padx=20, pady=15, anchor='w')
+        
+        Label(self.frame1, text="Username: ").grid(row=0, column=0, sticky='w')
+        
+        self.user_input = Entry(self.frame1, bg="white", width=24)
+        self.user_input.grid( row=0, column=1, sticky='w')
+        self.user_input.focus_set()
+
+
+        Label(self.frame1, text="Password: ").grid(row=1, column=0, sticky='w')
+        
+        self.pass_input = Entry(self.frame1, bg="white", width=24, show="*")
+        self.pass_input.grid(row=1, column=1)
+        
 
 # B = tk.Button(frame1, text ="Submit", font=40, command = lambda: helloCallBack(entry.get()) )
 # B.place(relx = 0.7, rely=0, relwidth=0.3, relheight=1)
 
-        self.frame2 = Frame(self.master, bg="black", bd=10)
-        self.frame2.place(relx=0.5, rely=0.2, relwidth=0.9, relheight=0.1, anchor='n')
+        
 
+        # self.btnlogin = Button(self.frame2, text="login")
+        # self.btnlogin.place(relwidth=0.7, relheight=1)
 
-        self.btnlogin = Button(self.frame2, text="login")
-        self.btnlogin.place(relwidth=0.7, relheight=1)
-
-        self.btnreg = Button(self.frame2, text="register")
-        self.btnreg.place(relx=0.75, rely=0.1, relwidth=0.25, relheight=1)
+        # self.btnreg = Button(self.frame2, text="register")
+        # self.btnreg.place(relx=0.75, rely=0.1, relwidth=0.25, relheight=1)
 
 
 
--- code/tempCodeRunnerFile.py
+++ code/tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-f.user_input = Entry(self.frame, bg="white", font=40)
-        # self.user_input.place(relwidth=0.45, relheight=1)
-        # self.user_in
+ tkinter
